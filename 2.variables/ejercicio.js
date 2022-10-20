0c4a970aac241c8afa3d59971bf7075ff907de33
merida <meridaincaespinoza@gmail.com>
2022-10-20 08:41:15 -0500

    ejercicio de tipos de datoy y variables

---
+++ 2.variables/ejercicio.js
@@ -0,0 +1,10 @@
+//averihuar la diferencia entre let y bar
+//respuesta
+//el variable declara con la palabra reservada
+// var es su contexto de ejecucion en curso
+// que puede ser la funcion que la contiene 
+//let permite aclara variables  limitados 
+//su alcanse scope 
+//var
+//define una variable  global o local en una 
+//funcion sin inportar el ambito del bloque 
