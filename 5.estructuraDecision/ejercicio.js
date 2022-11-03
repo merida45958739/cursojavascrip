7ddee57166c5a1fb5f1d9c0d61a43b7a87b3feb8
merida <meridaincaespinoza@gmail.com>
2022-11-03 10:44:47 -0500

    ejercicio del dia jueves 03 de noviembre

---
+++ 5.estructuraDecision/ejercicio.js
@@ -0,0 +1,89 @@
+
+/ otra manera numerico y mes  
+numeroMes=5
+switch(numeroMes){
+    case 1:
+        console.log("enero")
+        break
+        console.log("febrero")
+        break
+        case 3:
+            console.log("marzo")
+            break
+            default:
+                console.log("error mes no encontrado")
+}
+
+
+//otra manera en vocales 
+numeroMes=b
+switch(numeroMes){
+    case a:
+        console.log("es una vocal")
+        break
+        default:
+        console.log("no una vocal")
+
+
+
+
+//otra manera en vocales 
+        vocal="e"
+        switch(vocal){
+            case "a": case "e": case "i": case "o": case "u":
+                console.log ("la letra ${vocal},es una vocal")
+                break
+                default:
+                    console,log("no es una vocal")
+        }
+
+
+
+        // ejemplos  de otra manera de realizar la programacion
+        console.log ("la letra ${vocal},es una vocal")
+        console.log ("la letra ",vocal,"es una vocal")
+        console.log ("la letra " + vocal +",es una vocal")
+
+
+
+
+        // para realizar un ejercicio que te salga solo numeros pares de 2 a 20
+
+        // suma de numeros pares con console
+        for(let inicial=2;inicial<=20;inicial+=2){
+            console.log (inicial)
+        }
+
+
+        //ejemplo que se realice los numeros pares if
+        for(let inicial=1;inicial<=20;inicial++){
+        if (inicial%2==0){
+            console.log(i)
+            
+            // si deseamos ver los numeros inpares de 1 a 19
+        }else{
+            console.log(i)
+
+        }
+
+        //como mostrar en vocales "aeiou" y cuantas letras lleva  
+        
+
+        let vocales ="a;e;i;o;u"
+        console.log(vocales.length)
+        console .log (vocales)[2]
+        for(let i=0;i<=(vocales.length);i++){
+            console.log(vocales[i])
+        }
+
+
+        //realizar let de texto  y for cuantas vocales "a" existe en ese texto
+
+
+        let texto ="bienbenidos a puquio querido"
+        console.log(vocales.length)
+        console .log (vocales)[a]
+        for(let i=0;i<=(vocales.length);i++){
+            console.log(vocales[i])
+        }
+        
