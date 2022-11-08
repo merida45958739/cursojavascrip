b1302e0ead6c739e6141dd4502b49f577462bf95
merida <meridaincaespinoza@gmail.com>
2022-11-08 08:23:42 -0500

    tarea del dia martes

---
+++ 7.funciones/index.js
@@ -0,0 +1,7 @@
+// arrow function o funciones flecha
+//
+//pasos para contruir una function
+//1 palabra reservada  function
+//2 ponerle un nombre que escriba el trabajo
+//que realizara las funciones 
+//3 
