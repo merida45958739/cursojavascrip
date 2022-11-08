c80e7c81aa2fc37ce0f23f23ffb439a6225f023b
merida <meridaincaespinoza@gmail.com>
2022-11-08 10:52:43 -0500

    tarea del dia martes 8 de noviembre

---
+++ 7.funciones/ejercicio.js
@@ -0,0 +1,27 @@
+//funcion contar vocales(texto){retur cantidadVocales}
+contarVocales("eucalipto")//5
+let texto="eucalipto"
+let contador=0
+for (let i=0;i<texto.length;i++){
+    switch(texto^[i]){
+        case"a":case"e":case"i":case"o":case"u":
+        ++conrador
+    }
+
+}
+//funcion calculadora(num1,num2,operation){retur total}calculadora(45,12,"suma")//33
+function calcular(num1,num2,operacion){
+    if(operacion=="suma"){
+        total=num1+num2
+    }
+    if(operacion=="resta"){
+        total=num1+num2
+    } 
+    if(operacion=="multiplicacion"){
+        total=num1+num2
+    }
+    if(operacion=="division"){
+        total=num1+num2
+    }
+    return total
+}
--- 7.funciones/index.js
+++ 7.funciones/index.js
@@ -4,4 +4,60 @@
 //1 palabra reservada  function
 //2 ponerle un nombre que escriba el trabajo
 //que realizara las funciones 
-//3 
+//3 la funcion puede resivir parametros
+//pero si o si indicar que tendra parametros 
+//()
+//(listaAmigos)
+//(edad,nombre)
+//4 DATO IMPORTANTE.una funcion siempre tiene que 
+//retornar un tipo de dato
+
+function saludo (nombre,apellido){
+    let saludo="hola como estas "+nombre+" "+apellido
+    return saludo
+}
+
+//par usar una funcion debe llamar a la funcion 
+console.log(saludo("merita","isabel"))
+console.log(saludo("alex","cuadros"))
+
+
+
+// si quiero saber nombre y tambien apellido
+function saludo2 (nombre,apellido){
+    let saludo=""+nombre+""+apellido 
+    return saludo
+
+}
+//funciones
+console.log(saludo2("merida ","inca"))
+// tarea
+switch("num1+num2")
+{
+    case "1":
+    num1=ingresedatouno();
+    num2=ingresedatodos();
+    suma (num1,num2);
+    break;
+    case "2":
+    num1=ingresedatouno();
+    num2=ingresedatodos();
+    multiplicacion (num1,num2);
+    break; 
+    case "3":
+    num1=ingresedatouno();
+    num2=ingresedatodos();
+    resta (num1,num2);
+    break;
+    case "4":
+    num1=ingresedatouno();
+    num2=ingresedatodos();
+   division (num1,num2);
+    break;
+    case "5":
+    num1=ingresedatouno();
+    num2=ingresedatodos();
+    suma (num1,num2);
+    break;
+}
+retur=resultado;
